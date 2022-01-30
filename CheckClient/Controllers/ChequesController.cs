539a6afcf32a25f40e859e3370abc2de352e06d2
Braytiner <braytiner@hotmail.com>
2022-01-29 21:01:32 -0300

    Commit inicial

---
+++ CheckClient/Controllers/ChequesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheckClient.Controllers
+{
+    [Route("api/cheques")]
+    [ApiController]
+    public class ChequesController : ControllerBase
+    {
+        // GET: api/<ChequesController>
+        [HttpGet(Name = "ListarTodosCheques")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{cpf}", Name = "ListarPorCliente")]
+        public IEnumerable<string> Get(string cpf)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ChequesController>/5
+        [HttpGet("{id}", Name = "ListarChequePorId")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ChequesController>
+        [HttpPost(Name = "AdicionarCheque")]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ChequesController>/5
+        [HttpPut("{id}", Name = "AlterarCheque")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ChequesController>/5
+        [HttpDelete("{id}", Name = "ApagarCheque")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CheckClient/Controllers/ClientesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheckClient.Controllers
+{
+    [Route("api/clientes")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET: api/<ClientesController>
+        [HttpGet(Name = "ListarTodos")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{cpf}", Name = "ExibirCreditoRestante")]
+        public string Get(string cpf)
+        {
+            return "value";
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}", Name = "ListarPorId")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost(Name = "Adicionar")]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}", Name = "Alterar")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}", Name = "Apagar")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
